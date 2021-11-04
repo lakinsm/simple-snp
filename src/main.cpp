4b5f80c545f6bc9e9dfc61e07458e0b6e4b32a45
lakinsm <steven.m.lakin@gmail.com>
2021-11-04 11:59:52 -0500

    VCF output

--- src/main.cpp
+++ src/main.cpp
@@ -208,10 +208,10 @@ int main(int argc, const char *argv[]) {
             for(int i = 0; i < population_allele_counts.size(); ++i) {
                 double this_allele_freq = (double)x.second[i][j] / (double)sample_depth;
                 int ref_allele_count;
-                double ref_qual = 0;
+                double ref_qual;
                 if(this_nucleotides[i] == fasta_parser.seq[j]) {
                     ref_allele_count = x.second[i][j];
-                    ref_qual += (double)concurrent_q->all_qual_sums.at(x.first)[i][j];
+                    ref_qual = (double)concurrent_q->all_qual_sums.at(x.first)[i][j];
                     vcf_line_data.mqmr += (double)concurrent_q->all_mapq_sums.at(x.first)[i][j];
                 }
                 if((this_allele_freq >= args.min_minor_freq) && (x.second[i][j] >= args.min_intra_sample_alt)) {
@@ -238,7 +238,12 @@ int main(int argc, const char *argv[]) {
                     var_info += ",";
                     var_info += std::to_string(ref_allele_count);
                     var_info += ",";
-                    var_info += std::to_string(ref_qual / (double)ref_allele_count);
+                    if(ref_allele_count > 0) {
+                        var_info += std::to_string(ref_qual / (double)ref_allele_count);
+                    }
+                    else {
+                        var_info += ".";
+                    }
                     q.emplace(this_allele_freq, var_info);
 
                     vcf_line_data.ro += ref_allele_count;
@@ -388,17 +393,28 @@ int main(int argc, const char *argv[]) {
                         final_vcf_info += ',' + std::to_string(x.second[sample_nucl_idx][j]);
                     }
                     sample_nucl_idx = this_nucleotides.find(alts_present_at_pos[0]);
-                    final_vcf_info += ":" + std::to_string((double)concurrent_q->all_qual_sums.at(x.first)[sample_nucl_idx][j] /
-                            (double)x.second[sample_nucl_idx][j]);
+                    if(x.second[sample_nucl_idx][j] > 0) {
+                        final_vcf_info += ":" + std::to_string((double)concurrent_q->all_qual_sums.at(x.first)[sample_nucl_idx][j] /
+                                                               (double)x.second[sample_nucl_idx][j]);
+                    }
+                    else {
+                        final_vcf_info += ":.";
+                    }
+
                     for(int i = 1; i < vcf_line_data.ao.size(); ++i) {
                         sample_nucl_idx = this_nucleotides.find(alts_present_at_pos[i]);
-                        final_vcf_info += ',' + std::to_string((double)concurrent_q->all_qual_sums.at(x.first)[sample_nucl_idx][j] /
-                                (double)x.second[sample_nucl_idx][j]);
+                        final_vcf_info += ',';
+                        if(x.second[sample_nucl_idx][j] > 0) {
+                            final_vcf_info += std::to_string((double)concurrent_q->all_qual_sums.at(x.first)[sample_nucl_idx][j] /
+                                    (double)x.second[sample_nucl_idx][j]);
+                        }
+                        else {
+                            final_vcf_info += '.';
+                        }
                     }
                 }
                 else {
-                    final_vcf_info += ro + ',' + ao + ":" + ro + ":" + qr + ":" + ao + ":";
-                    final_vcf_info += qa;
+                    final_vcf_info += ro + ',' + ao + ":" + ro + ":" + qr + ":" + ao + ":" + qa;
                 }
 
                 vcf_line_data.ns++;
