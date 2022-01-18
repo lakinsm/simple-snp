4e0e2085bc27fb05cc2b7260e185a382c78ebc05
lakinsm <steven.m.lakin@gmail.com>
2022-01-18 14:58:18 -0600

    Begin devel on indel finder

--- src/args.h
+++ src/args.h
@@ -24,7 +24,7 @@ public:
     int min_intra_sample_depth = 5;
     int min_inter_sample_depth = 10;
     int min_large_indel_len = 75;
-    int large_indel_max_window_depth = 1;
+    int large_indel_max_window_depth = 2;
     int indel_accel_window_size = 3;
     double large_indel_border_ratio = 0.2;
     double min_major_freq = 0.7;
